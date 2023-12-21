56aa97bf837e50ad6aa59ff65bbfa33d430ffb46
oskarhlm <oskar.2303@gmail.com>
2023-12-21 15:36:59 +0100

    Add map

--- src/frontend/src/components/Map/Map.tsx
+++ src/frontend/src/components/Map/Map.tsx
@@ -1,5 +1,20 @@
 import styles from './styles.module.css';
+import maplibregl from 'maplibre-gl';
+
+import 'maplibre-gl/dist/maplibre-gl.css';
+import { onMount } from 'solid-js';
 
 export default function Map() {
-  return <div class={styles.map}></div>;
+  let mapRef: HTMLDivElement | undefined;
+
+  onMount(() => {
+    const map = new maplibregl.Map({
+      container: mapRef as HTMLElement,
+      style: 'https://demotiles.maplibre.org/style.json', // style URL
+      center: [139.753, 35.6844],
+      zoom: 3,
+    });
+  });
+
+  return <div id={styles['map']} ref={mapRef} />;
 }
