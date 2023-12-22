e527f2bb293f073cdbdab14a0ce46ceb8538dd23
oskarhlm <oskar.2303@gmail.com>
2023-12-22 12:21:59 +0100

    Add simple FastAPI server

---
+++ src/backend/server.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
+import interpreter
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = FastAPI()
+
+@app.get("/chat")
+def chat_endpoint(message: str):
+    def event_stream():
+        for result in interpreter.chat(message, stream=True):
+            yield f"data: {result}\n\n"
+
+    return StreamingResponse(event_stream(), media_type="text/event-stream")
+
+@app.get("/history")
+def history_endpoint():
+    return interpreter.messages
