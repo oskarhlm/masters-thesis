e2f5246a2428fba192e7b43e346a179159aba02d
oskarhlm <oskar.2303@gmail.com>
2024-01-24 11:54:58 +0100

    Start writing about datasets

--- src/langchain/lib/agents/sessions.py
+++ src/langchain/lib/agents/sessions.py
@@ -2,13 +2,15 @@ import os
 import uuid
 from langchain_community.chat_message_histories import RedisChatMessageHistory
 from langchain.chains.conversation.memory import ConversationBufferWindowMemory
+from datetime import datetime
 
 MEMORY_KEY = 'chat_memory'
 
 
 def get_session(session_id: str = None):
     if not session_id:
-        session_id = f'session-{uuid.uuid4()}'
+        current_date = datetime.now().strftime("%Y%m%d")  # Format: YYYYMMDD
+        session_id = f'session-{uuid.uuid4()}-{current_date}'
 
     message_history = []
 
@@ -16,8 +18,7 @@ def get_session(session_id: str = None):
         if not os.getenv('REDIS_URL'):
             raise KeyError('No REDIS_URL set in Docker environment')
         message_history = RedisChatMessageHistory(
-            url=os.getenv('REDIS_URL'), session_id=session_id
-        )
+            url=os.getenv('REDIS_URL'), session_id=session_id)
 
     memory = ConversationBufferWindowMemory(
         k=6, memory_key=MEMORY_KEY, chat_memory=message_history, return_messages=True, input_key='input', output_key='output')
