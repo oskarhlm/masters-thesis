6a8906783736d0c8dab3ab78784db460a78d7e77
oskarhlm <oskar.2303@gmail.com>
2024-02-07 15:43:42 +0100

    Get SQL agent to output GeoJSON

--- src/langchain/lib/agents/sql_agent.py
+++ src/langchain/lib/agents/sql_agent.py
@@ -1,24 +1,59 @@
-from langchain_core.prompts.chat import (
-    ChatPromptTemplate,
-    HumanMessagePromptTemplate,
-    MessagesPlaceholder,
-)
 from langchain_core.messages import AIMessage, SystemMessage
 from langchain_community.agent_toolkits.sql.prompt import SQL_FUNCTIONS_SUFFIX
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder, HumanMessagePromptTemplate
-from langchain_core.messages import SystemMessage
 from langchain.agents import create_openai_tools_agent, AgentExecutor
-
-from .sessions import MEMORY_KEY, get_session
 from langchain_community.agent_toolkits import SQLDatabaseToolkit
-from langchain_openai import ChatOpenAI
-from langchain.agents import create_openai_tools_agent
-from langchain.agents.agent import AgentExecutor
+from langchain.sql_database import SQLDatabase
+from langchain_community.tools.sql_database.tool import BaseSQLDatabaseTool, ListSQLDatabaseTool, InfoSQLDatabaseTool, QuerySQLCheckerTool
+from langchain_core.tools import BaseTool
 
 from ..tools.map_interaction.geojson_url import GeoJSONURLTool
+from .sessions import MEMORY_KEY, get_session
 
-from langchain.sql_database import SQLDatabase
+import json
+
+
+collection_query_template = """
+SELECT jsonb_build_object(
+    'type',     'FeatureCollection',
+    'features', jsonb_agg(features.feature)
+)
+FROM (
+  SELECT jsonb_build_object(
+    'type',       'Feature',
+    'id',         id,
+    'geometry',   ST_AsGeoJSON(geom)::jsonb,
+    'properties', to_jsonb(inputs) - 'id' - 'geom'
+  ) AS feature
+  FROM ({dynamic_query}) inputs) features;
+"""
+
+
+class CustomQuerySQLDataBaseTool(BaseSQLDatabaseTool, BaseTool):
+    """Tool for querying a SQL database."""
+
+    name: str = "sql_database_query"
+    description: str = """
+    Input to this tool is a detailed and correct SQL query, output is a result from the database.
+    If the query is not correct, an error message will be returned.
+    If an error is returned, rewrite the query, check the query, and try again.
+    The tool will return the rows  as a GeoJSON FeatureCollection. 
+    """
+
+    def _run(
+        self,
+        query: str,
+    ) -> str:
+        """Execute the query, return the results or an error message."""
+        collection_query = collection_query_template.format(
+            dynamic_query=query.strip(';'))
+        result = self.db.run_no_throw(collection_query, include_columns=True)
+        return json.loads(result.replace("'", '"').replace('None', 'null'))[0]['jsonb_build_object']
+
+
+AI_SUFFIX = """I should look at the tables in the database to see what I can query.  
+Then I should query the schema of the most relevant tables."""
 
 
 def create_sql_agent(session_id: str = None):
@@ -30,7 +65,7 @@ def create_sql_agent(session_id: str = None):
             MessagesPlaceholder(variable_name=MEMORY_KEY),
             HumanMessagePromptTemplate.from_template("{input}"),
             # AIMessage(content=SQL_FUNCTIONS_SUFFIX),
-            AIMessage(content='hmmmm'),
+            AIMessage(content=AI_SUFFIX),
             MessagesPlaceholder(variable_name="agent_scratchpad"),
         ]
     )
@@ -40,11 +75,15 @@ def create_sql_agent(session_id: str = None):
 
     toolkit = SQLDatabaseToolkit(db=db, llm=ChatOpenAI(temperature=0))
     context = toolkit.get_context()  # Context not currently included in prompt
-    tools = [*toolkit.get_tools(), GeoJSONURLTool()]
+    tools = [*toolkit.get_tools(), GeoJSONURLTool(),
+             CustomQuerySQLDataBaseTool(db=db)]
+    tools = list(filter(lambda x: x.name != 'sql_db_query', tools))
+    print([tool.name for tool in tools])
 
     prompt = prompt.partial(**context)
 
     session_id, memory = get_session(session_id)
+
     # llm = ChatOpenAI(model="gpt-4-1106-preview", temperature=0,
     #                  streaming=True)
     llm = ChatOpenAI(model="gpt-3.5-turbo-1106", temperature=0, streaming=True)
